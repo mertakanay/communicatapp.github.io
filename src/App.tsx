32457f1f1f22713c144313b4657ac5ce2d82efd4
Mert Akanay <>
2025-07-01 20:24:03 +0100

    new updates

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Hero from "./components/HeroSection";
+import Description from "./components/Description";
+import CoreFeatures from "./components/CoreFeatures";
+import HowItWorks from "./components/HowItWorks";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="bg-[#0d1b2a] min-h-screen w-full font-sans">
+      <Hero />
+      <Description />
+      <CoreFeatures />
+      <HowItWorks />
+      <CallToAction />
+      <Footer />
+    </main>
+  );
+}
